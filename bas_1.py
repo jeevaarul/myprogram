db3d8359ac2a1f2b394ff092bcc77739bc823bc8
jeevaarul <jeevaarul814@gmail.com>
2019-05-01 10:57:15 +0530

    first commit

---
+++ bas_1.py
@@ -0,0 +1,4 @@
+#Python Program to Add Two Numbers
+a=input("enter your first number:")
+b=input("enter your second number:")
+print(a+b)
---
+++ bas_2.py
@@ -0,0 +1,3 @@
+#Python Program to Find the Square Root(a)1/2)
+a=input("enter the values:")
+print(a**0.5)
