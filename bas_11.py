bcf80f02be975641f6e7a9a9479a22b576178855
jeevaarul <jeevaarul814@gmail.com>
2019-05-04 11:11:23 +0530

    postive or negative

---
+++ bas_11.py
@@ -0,0 +1,8 @@
+#Python Program to Check if a Number is Positive, Negative or 0
+a=input("enter the number:")
+if(a>0):
+    print("postive")
+elif(a<0):
+    print("negative")
+else:
+     print("zero")
