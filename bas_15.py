552659fa24ea4ca9a2437547deb3a456a694c71d
jeevaarul <jeevaarul814@gmail.com>
2019-05-04 11:51:42 +0530

    smallest number 3

---
+++ bas_15.py
@@ -0,0 +1,11 @@
+#Python Program to Find the Smallest Among Three Numbers
+
+a=.3
+b=4
+c=2
+if(a<b)and(a<c):
+    print("a is smallest number")
+elif(b<a)and(b<c):
+    print("b is smallest number")
+else:
+    print("c is smallest number")
