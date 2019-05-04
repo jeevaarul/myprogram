8da9f74deaf3e04b5c22d7242b3b800e0eceef5d
jeevaarul <jeevaarul814@gmail.com>
2019-05-04 11:45:05 +0530

    biggest 3 numbers

---
+++ bas_14.py
@@ -0,0 +1,10 @@
+#Python Program to Find the Largest Among Three Numbers
+a=30
+b=4
+c=2
+if(a>b)and(a>c):
+    print("a is bigest number")
+elif(b>a)and(b>c):
+    print("b is bigest number")
+else:
+    print("c is bigest number")
